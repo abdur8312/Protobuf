e283163426d83da0a1b7e35b34c002e56b995605
AbdurPC <abdurrahman100800@gmail.com>
2023-05-01 17:58:22 +0530

    added some proto files

--- src/main/java/com/rahman/ProtobufExample2/App.java
+++ src/main/java/com/rahman/ProtobufExample2/App.java
@@ -1,26 +1,18 @@
 package com.rahman.ProtobufExample2;
 
 import com.abdur8312.protoFiles.Hello;
-import com.abdur8312.protoFiles.Hello.HelloWorld;
-import com.abdur8312.protoFiles.Personal;
-import com.abdur8312.protoFiles.Personal.Information;
-import com.abdur8312.protoFiles.WorldOuterClass;
-import com.abdur8312.protoFiles.WorldOuterClass.World;
+//import com.abdur8312.protoFiles.;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        
-        
-        Hello.HelloWorld a = HelloWorld.newBuilder().setMessage("Hello world!").build();
+
+        Hello.HelloWorld a = Hello.HelloWorld.newBuilder().setMessage("Hello world!").build();
         System.out.println(a.getMessage());
-        
-        Personal.Information b = Information.newBuilder().setId(1).setName("abdur").setCity("chennai").build();
-        System.out.println(b.toString());
-        
-        WorldOuterClass.World c = World.newBuilder().setFName("ABDUR").setLName("RAHMAN").build();
-        System.out.println(c.toString());
-        
+
+
+
+
     }
 }
